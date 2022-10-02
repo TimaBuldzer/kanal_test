3c2fb2f58c569aa1cacdfc52d2b6e89af0adc168
TimaBuldzer <timamasharipov47@gmail.com>
2022-10-02 12:42:16 +0300

    change: - project structure.
    add:
    - google_api, tsb_rf, orders, users apps(initial).

---
+++ apps/tsb_rf/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TsbRfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.tsb_rf'
--- conf/urls.py
+++ conf/urls.py
@@ -1,4 +1,4 @@
-"""kanal_test URL Configuration
+"""conf URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
